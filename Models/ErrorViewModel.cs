d6b23456ec6f74dd3554dbfaf2c0ae1b70c98b1e
Armando <Armando@DESKTOP-9TFU221>
2023-11-20 09:38:57 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BowlThemUp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
