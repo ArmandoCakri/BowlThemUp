696a9819013d94ecd8892f4dcb3d0ea0c9f10e4c
Armando <124374370+ArmandoCakri@users.noreply.github.com>
2023-11-27 15:06:13 +0000

    hover/img

---
+++ Controllers/EventController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BowlThemUp.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult christmas()
+        {
+            return View();
+        }
+    }
+}
